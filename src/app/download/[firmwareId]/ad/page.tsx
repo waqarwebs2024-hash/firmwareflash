bebf71070b11e01bb22eba9443a7f6c446be5ead
Muhammad Waqar Asghar <waqarwebs2024@gmail.com>
2025-09-21 09:00:53 +0000

    Try fixing this error: `Console Error: A param property was accessed dir

--- src/app/download/[firmwareId]/ad/page.tsx
+++ src/app/download/[firmwareId]/ad/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, use } from 'react';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { getAdSettings } from '@/lib/data';
