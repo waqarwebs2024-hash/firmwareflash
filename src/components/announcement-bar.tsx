d7eb1bd014b63b6957bc183d87cbc45c62ba474e
Muhammad Waqar Asghar <waqarwebs2024@gmail.com>
2025-09-22 17:37:55 +0000

    Announcement bar ma text move na ho. Test ka size bara chota hoo. Aor an

--- src/components/announcement-bar.tsx
+++ src/components/announcement-bar.tsx
@@ -1,13 +1,13 @@
 export function AnnouncementBar({ announcement }: { announcement: string | null }) {
-    if (!announcement) return null;
+    const displayAnnouncement = announcement || 'Now You Can Download Firmware Free On firmwareflash.com';
   
     return (
       <div className="rainbow-border">
-        <div className="rainbow-border-content text-primary-foreground text-sm overflow-hidden">
+        <div className="rainbow-border-content text-primary-foreground text-center text-sm overflow-hidden">
             <div className="container mx-auto px-4">
-            <div className="relative h-8 flex items-center">
-                <p className="marquee">{announcement}</p>
-            </div>
+                <div className="relative h-8 flex items-center justify-center">
+                    <p className="pulse-text-animation">{displayAnnouncement}</p>
+                </div>
             </div>
         </div>
       </div>
